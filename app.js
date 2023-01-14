b871a856fbb1e1adf476ae62df9198b250a650d1
Edith505 <edithrand505@gmail.com>
2023-01-14 19:29:24 +0300

    sendFile Express

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express()
 const port = 3000
 
+//Affichage de la page d'acceuille dans le '/'
 app.get('/', (req, res) =>{
-    res.send('hello world')
+    //Afficher un fichier existant
+    res.sendFile(__dirname + '/index.html')
+}) 
+//Affichage d'une page contact dans l'adresse /contact
+app.get('/contact', (req, res) =>{
+    //Afficher un fichier existant
+    res.sendFile(__dirname + '/contact.html')
 }) 
 app.listen(port, () =>{
     console.log(`app demarrer sur le port ${port}`);
