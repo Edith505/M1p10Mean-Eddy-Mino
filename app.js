b2e55a18d95a81baf85a8f176664dc734e128cd1
Edith505 <edithrand505@gmail.com>
2023-01-14 21:31:57 +0300

    Moteur de template twig

--- app.js
+++ app.js
@@ -4,22 +4,29 @@ const port = 3000
 
 //Ajout d'un fichier static comme Css/javascrip ou des images
 app.use(express.static('public'))
+
+//Utilisation template twig
+app.set("view engine", "twig")
+
 //utilisation middlewares avec use
 app.use('/', (req, res, next) =>{  
     //exemple d'un middlwares
-    console.log("middlewares");
+    console.log("middlewares succès");
     next();
 }) 
 
 //Affichage de la page d'acceuille dans le path'/'
 app.get('/', (req, res) =>{
     //Afficher un fichier existant
-    res.sendFile(__dirname + '/index.html')
+   // res.sendFile(__dirname + '/index.html')
+   //methode render
+    res.render('index', {title: "Home Page"});
 }) 
 //Affichage d'une page contact dans l'adresse /contact
 app.get('/contact', (req, res) =>{
     //Afficher un fichier existant
-    res.sendFile(__dirname + '/contact.html')
+    //res.sendFile(__dirname + '/contact.html')
+    res.render('contact');
 })
 
 //ecouter sur le port l'app 3000
